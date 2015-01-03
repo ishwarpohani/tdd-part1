571d643e76573e970551c6a5704730263fe08b63
Ishwar Pohani <ishwar_dskd@yahoo.com>
2015-01-03 21:06:41 +0500

    // Chapter 9. Times We're Livin' In (Completed)

--- tdd-part1/src/test/MultiCurrencyMoneyTest.java
+++ tdd-part1/src/test/MultiCurrencyMoneyTest.java
@@ -13,66 +13,6 @@ import currency.Money;
 
 public class MultiCurrencyMoneyTest {
 	
-	/** Chapter 1. Multi-Currency Money (Test Case) */
-//	@Test
-//	public void testMultiplicationCH1() {
-//		Dollar five= new Dollar(5);
-//		five.times(2);
-//		assertEquals(10, five.amount);
-//	}
-	
-	/** Chapter 2. Degenerate Objects (Test Case) */	
-//	@Test
-//	public void testMultiplicationCH2() {
-//		Dollar five= new Dollar(5);
-//		Dollar product= five.times(2);
-//		assertEquals(10, product.amount);
-//		product= five.times(3);
-//		assertEquals(15, product.amount);
-//	}
-	
-	/** Chapter 3. Equality for All (Test Case) */
-	@Test
-	public void testEqualityCH3() {
-		assertTrue(new Dollar(5).equals(new Dollar(5)));
-		assertFalse(new Dollar(5).equals(new Dollar(6)));
-	}
-	
-	/** Chapter 4. Privacy (Test Case) */
-	@Test
-	public void testMultiplicationCH4() {
-		Dollar five= new Dollar(5);
-		assertEquals(new Dollar(10), five.times(2));
-		assertEquals(new Dollar(15), five.times(3));
-	}
-	
-	/** Chapter 5. Franc-ly Speaking (Test Case) */
-	@Test
-	public void testFrancMultiplicationCH5() {
-		Franc five= new Franc(5);
-		assertEquals(new Franc(10), five.times(2));
-		assertEquals(new Franc(15), five.times(3));
-	}
-	
-	/** Chapter 6. Equality for All, Redux (Test Case) */
-	@Test
-	public void testEqualityCH6() {
-		assertTrue(new Dollar(5).equals(new Dollar(5)));
-		assertFalse(new Dollar(5).equals(new Dollar(6)));
-		assertTrue(new Franc(5).equals(new Franc(5)));
-		assertFalse(new Franc(5).equals(new Franc(6)));
-	}
-	
-	/** Chapter 7. Apples and Oranges (Test Case) */
-	@Test
-	public void testEqualityCH7() {
-		assertTrue(new Dollar(5).equals(new Dollar(5)));
-		assertFalse(new Dollar(5).equals(new Dollar(6)));
-		assertTrue(new Franc(5).equals(new Franc(5)));
-		assertFalse(new Franc(5).equals(new Franc(6)));
-		assertFalse(new Franc(5).equals(new Dollar(5)));
-	}
-	
 	/** Chapter 8. Makin' Objects (Test Case) */
 	@Test
 	public void testMultiplication() {
@@ -97,6 +37,75 @@ public class MultiCurrencyMoneyTest {
 		assertEquals(Money.franc(15), five.times(3));
 	}
 	
+	/** Chapter 9. Times We're Livin' In (Test Case) */
+	@Test
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
+	}
+	
+	
+//	/** Chapter 1. Multi-Currency Money (Test Case) */
+//	@Test
+//	public void testMultiplicationCH1() {
+//		Dollar five= new Dollar(5);
+//		five.times(2);
+//		assertEquals(10, five.amount);
+//	}
+	
+//	/** Chapter 2. Degenerate Objects (Test Case) */	
+//	@Test
+//	public void testMultiplicationCH2() {
+//		Dollar five= new Dollar(5);
+//		Dollar product= five.times(2);
+//		assertEquals(10, product.amount);
+//		product= five.times(3);
+//		assertEquals(15, product.amount);
+//	}
+
+//	/** Chapter 3. Equality for All (Test Case) */
+//	@Test
+//	public void testEqualityCH3() {
+//		assertTrue(new Dollar(5).equals(new Dollar(5)));
+//		assertFalse(new Dollar(5).equals(new Dollar(6)));
+//	}
+//	
+//	/** Chapter 4. Privacy (Test Case) */
+//	@Test
+//	public void testMultiplicationCH4() {
+//		Dollar five= new Dollar(5);
+//		assertEquals(new Dollar(10), five.times(2));
+//		assertEquals(new Dollar(15), five.times(3));
+//	}
+//	
+//	/** Chapter 5. Franc-ly Speaking (Test Case) */
+//	@Test
+//	public void testFrancMultiplicationCH5() {
+//		Franc five= new Franc(5);
+//		assertEquals(new Franc(10), five.times(2));
+//		assertEquals(new Franc(15), five.times(3));
+//	}
+	
+//	/** Chapter 6. Equality for All, Redux (Test Case) */
+//	@Test
+//	public void testEqualityCH6() {
+//		assertTrue(new Dollar(5).equals(new Dollar(5)));
+//		assertFalse(new Dollar(5).equals(new Dollar(6)));
+//		assertTrue(new Franc(5).equals(new Franc(5)));
+//		assertFalse(new Franc(5).equals(new Franc(6)));
+//	}
+//	
+//	/** Chapter 7. Apples and Oranges (Test Case) */
+//	@Test
+//	public void testEqualityCH7() {
+//		assertTrue(new Dollar(5).equals(new Dollar(5)));
+//		assertFalse(new Dollar(5).equals(new Dollar(6)));
+//		assertTrue(new Franc(5).equals(new Franc(5)));
+//		assertFalse(new Franc(5).equals(new Franc(6)));
+//		assertFalse(new Franc(5).equals(new Dollar(5)));
+//	}
+
+
 	
 }
 
