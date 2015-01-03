c7ec91260639b4bd681043c2e05498600f1d7adf
Ishwar Pohani <ishwar_dskd@yahoo.com>
2015-01-03 20:45:00 +0500

    // Chapter 8. Makin' Objects (Completed)

--- tdd-part1/src/test/MultiCurrencyMoneyTest.java
+++ tdd-part1/src/test/MultiCurrencyMoneyTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import currency.Dollar;
 import currency.Franc;
+import currency.Money;
 
 
 public class MultiCurrencyMoneyTest {
@@ -39,7 +40,7 @@ public class MultiCurrencyMoneyTest {
 	
 	/** Chapter 4. Privacy (Test Case) */
 	@Test
-	public void testMultiplication() {
+	public void testMultiplicationCH4() {
 		Dollar five= new Dollar(5);
 		assertEquals(new Dollar(10), five.times(2));
 		assertEquals(new Dollar(15), five.times(3));
@@ -47,7 +48,7 @@ public class MultiCurrencyMoneyTest {
 	
 	/** Chapter 5. Franc-ly Speaking (Test Case) */
 	@Test
-	public void testFrancMultiplication() {
+	public void testFrancMultiplicationCH5() {
 		Franc five= new Franc(5);
 		assertEquals(new Franc(10), five.times(2));
 		assertEquals(new Franc(15), five.times(3));
@@ -64,13 +65,38 @@ public class MultiCurrencyMoneyTest {
 	
 	/** Chapter 7. Apples and Oranges (Test Case) */
 	@Test
-	public void testEquality() {
+	public void testEqualityCH7() {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
 		assertTrue(new Franc(5).equals(new Franc(5)));
 		assertFalse(new Franc(5).equals(new Franc(6)));
 		assertFalse(new Franc(5).equals(new Dollar(5)));
 	}
+	
+	/** Chapter 8. Makin' Objects (Test Case) */
+	@Test
+	public void testMultiplication() {
+		Money five = Money.dollar(5);
+		assertEquals(Money.dollar(10), five.times(2));
+		assertEquals(Money.dollar(15), five.times(3));
+	}
+	/** Chapter 8. Makin' Objects (Test Case) */
+	@Test
+	public void testEquality() {
+		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+		assertTrue(Money.franc(5).equals(Money.franc(5)));
+		assertFalse(Money.franc(5).equals(Money.franc(6)));
+		assertFalse(Money.franc(5).equals(Money.dollar(5)));
+	}
+	/** Chapter 8. Makin' Objects (Test Case) */
+	@Test
+	public void testFrancMultiplication() {
+		Money five = Money.franc(5);
+		assertEquals(Money.franc(10), five.times(2));
+		assertEquals(Money.franc(15), five.times(3));
+	}
+	
 	
 }
 
