0e5df33dd1bc735978b7ada72b3ac858fd19d4d5
Ishwar Pohani <ishwar_dskd@yahoo.com>
2015-01-03 17:45:13 +0500

    // Chapter 4. Privacy (completed)

--- tdd-part1/src/test/MultiCurrencyMoneyTest.java
+++ tdd-part1/src/test/MultiCurrencyMoneyTest.java
@@ -26,14 +26,14 @@ public class MultiCurrencyMoneyTest {
 	 * Chapter 2. Degenerate Objects
 	 * Test Case
 	 */	
-	@Test
-	public void testMultiplicationCH2() {
-		Dollar five= new Dollar(5);
-		Dollar product= five.times(2);
-		assertEquals(10, product.amount);
-		product= five.times(3);
-		assertEquals(15, product.amount);
-	}
+//	@Test
+//	public void testMultiplicationCH2() {
+//		Dollar five= new Dollar(5);
+//		Dollar product= five.times(2);
+//		assertEquals(10, product.amount);
+//		product= five.times(3);
+//		assertEquals(15, product.amount);
+//	}
 	
 	/**
 	 * Chapter 3. Equality for All
@@ -43,6 +43,16 @@ public class MultiCurrencyMoneyTest {
 	public void testEquality() {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
+	}
+	
+	/**
+	 * Chapter 4. Privacy
+	 * Test Case
+	 */
+	public void testMultiplication() {
+		Dollar five= new Dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
 	}
 	
 }
