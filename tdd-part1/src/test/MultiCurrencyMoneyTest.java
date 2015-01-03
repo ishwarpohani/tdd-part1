58883f3a5f72a666df1d90bb20e0552cc247dc0d
Ishwar Pohani <ishwar_dskd@yahoo.com>
2015-01-03 17:25:40 +0500

    // Chapter 2 : Degenerate Objects (Completed)

--- tdd-part1/src/test/MultiCurrencyMoneyTest.java
+++ tdd-part1/src/test/MultiCurrencyMoneyTest.java
@@ -9,11 +9,26 @@ import currency.Dollar;
 
 public class MultiCurrencyMoneyTest {
 	
+	/**
+	 * Chapter 1 Test Case
+	 */
+//	@Test
+//	public void testMultiplicationCH1() {
+//		Dollar five= new Dollar(5);
+//		five.times(2);
+//		assertEquals(10, five.amount);
+//	}
+	
+	/**
+	 * Chapter 2 Test Case
+	 */	
 	@Test
-	public void testMultiplication() {
+	public void testMultiplicationCH2() {
 		Dollar five= new Dollar(5);
-		five.times(2);
-		assertEquals(10, five.amount);
+		Dollar product= five.times(2);
+		assertEquals(10, product.amount);
+		product= five.times(3);
+		assertEquals(15, product.amount);
 	}
 	
 }
